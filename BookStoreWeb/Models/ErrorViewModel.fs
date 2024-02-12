bb04d6daf2edcc61bfe1a9d05b695abe94c2974d
İsmail Temüroğlu <ismailtemuroglu2819@gmail.com>
2024-02-12 17:48:41 +0300

    Proje dosyası ekle.

---
+++ BookStoreWeb/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace BookStoreWeb.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
